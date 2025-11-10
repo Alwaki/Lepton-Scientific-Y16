04dc33f4b61d29e00602e9d2e102443bfcc52109
alwaki <xalwa999@gmail.com>
2025-11-10 18:31:27 +0100

    updated

--- camera.py
+++ camera.py
@@ -3,25 +3,30 @@ import subprocess
 subprocess.run([
     "v4l2-ctl",
     "--device=/dev/video2",
-    "--set-fmt-video=width=160,height=120,pixelformat='Y16 '"
+    "--set-fmt-video=width=160,height=120,pixelformat=Y16 "
 ])
 device = "/dev/video2"
 
 # Query basic info
 info = subprocess.run(["v4l2-ctl", "--device", device, "--all"],
                       capture_output=True, text=True)
-print(info.stdout)
+#print(info.stdout)
 
 import cv2
 import numpy as np
 
+fourcc = cv2.VideoWriter.fourcc('Y','1','6',' ') # fourcc = four digit format codec
 cap = cv2.VideoCapture('/dev/video2')
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 120)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 160)
-cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('Y','1','6',' '))
+cap.set(cv2.CAP_PROP_FOURCC, fourcc)
 cap.set(cv2.CAP_PROP_CONVERT_RGB, 0)
 
+print("CAP_PROP_FOURCC:", cap.get(cv2.CAP_PROP_FOURCC))
+print("CAP_PROP_CONVERT_RGB:", cap.get(cv2.CAP_PROP_CONVERT_RGB))
 
+
+frame_buf = []
 
 while True:
     ret, frame = cap.read()
@@ -32,10 +37,13 @@ while True:
     data = frame.view(np.uint16)
 
     # For display only (do NOT use for quantitative analysis)
-    data = cv2.normalize(data, None, 0, 255, cv2.NORM_MINMAX)
-    data = np.uint8(data)
-    data = cv2.applyColorMap(data, cv2.COLORMAP_INFERNO)
+    #data = cv2.normalize(data, None, 0, 255, cv2.NORM_MINMAX)
+    #data = np.uint8(data)
+    #data = cv2.applyColorMap(data, cv2.COLORMAP_INFERNO)
     disp = cv2.resize(data, None, fx=4, fy=4, interpolation=cv2.INTER_NEAREST)
+    
+    # Write video to memory
+    frame_buf.append(data)
 
     # Use CV2 to show image
     cv2.imshow('Lepton Radiometric (Y16)', disp)
@@ -44,3 +52,9 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
+num    = 0
+folder = "/home/wallen/Documents/Lepton/capture"
+while len(frame_buf)>0:
+    cv2.imwrite(f'{folder}/cap_{num}.tiff', frame_buf.pop(0))
+    num += 1
